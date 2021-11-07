d491d628b9949a0d1d680815737ba0a40246211c
ɱ 2 ƙ <87993586+m22k@users.noreply.github.com>
2021-11-07 13:29:44 +0200

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,13 @@
+Locales['en'] = {
+    ['invalid_amount'] = 'Väärä summa',
+    ['atm_blip'] = 'ATM',
+    ['bank_blip'] = 'Pankki',
+    ['atm_open'] = 'Paina ~INPUT_PICKUP~ to access account ~b~',
+    ['no_money'] = 'Sinulal ei ole tarpeeksi rahaa',
+    ['transfer_money'] = 'Siirrä Raha',
+    ['bank'] = 'Pankki',
+    ['transfer_to'] = 'Sinä siirsit ~r~$ %s~s~ henkilölle ~r~%s.',
+    ['receive_money'] = 'Sait rahaa ~r~$ %s~s~ henkilöltä ~r~%s.',
+    ['not_to_yourself'] = 'Et voi siirtää rahaa itsellesi!',
+    ['not_enough_money'] = 'Ei tarpeeksi rahaa siirettäväksi'
+}
